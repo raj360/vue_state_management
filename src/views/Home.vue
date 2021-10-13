d1a16c26bd486fdc4aa69d5a23374fdb43008451
Raymond <rayjxclusive@gmail.com>
2021-10-13 11:56:36 +0300

    finalized setting up counter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <div class="counter">{{counter}}</div>
+    
+    <div class="buttons">
+     <button @click="increaseCounter">+</button>
+     <button @click="decreaseCounter">-</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+  return {
+   counter:0
+  }
+  },
+  methods:{
+  increaseCounter(){
+    this.counter++
+  },
+  decreaseCounter(){
+    this.counter--
+  },
   }
 }
 </script>
+
+<style >
+div{
+margin-bottom: 10px;
+}
+
+.counter{
+ font-size: 80px;
+}
+
+.buttons button{
+ font-size:40px;
+ width:100px;
+ margin:10px;
+}
+
+</style>
